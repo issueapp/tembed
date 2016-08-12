01f89621a76b5553984d873436a2a12ed860c53b
khoa nguyen <huukhoanguyen@hotmail.com>
2016-08-12 14:09:51 +1000

    load correct ext for linux and mac os

--- lib/tembed.rb
+++ lib/tembed.rb
@@ -1,4 +1,9 @@
-require_relative './tembed.bundle'
+case RUBY_PLATFORM
+when /linux/
+  require_relative './tembed.so'
+when /darwin/
+  require_relative './tembed.bundle'
+end
 
 module Tembed
   VERSION = '0.0.2'
