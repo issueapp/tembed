771868dbd500b7c1178ce4d9a726160e86713a6d
Khoa Nguyen <khoan@Khoas-MacBook-Pro.local>
2017-06-11 02:36:59 +1000

    use pure ruby where it makes sense

--- lib/tembed.rb
+++ lib/tembed.rb
@@ -1,1 +1,22 @@
 require_relative './native/tembed'
+
+module Tembed
+  def self.call thing
+    require 'tempfile'
+    f = Tempfile.new(['tembed', '.ttf'])
+
+    if is_data = thing.respond_to?(:start_with?) && ! thing.start_with?('/')
+      IO.write(f.path, thing, mode: 'w', encoding: thing.encoding)
+    else
+      require 'fileutils'
+      FileUtils.cp(thing, f.path)
+    end
+
+    call! f
+
+    yield f.read
+
+    f.close
+    f.unlink
+  end
+end
